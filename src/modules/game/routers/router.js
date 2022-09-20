a286dd677f48943feff1b50deff0f39c0a2958bc
liziwei <alssylk@gmail.com>
2022-09-20 08:43:02 -0400

    use get method

--- src/modules/game/routers/router.js
+++ src/modules/game/routers/router.js
@@ -2,13 +2,13 @@
  * @Author: liziwei01
  * @Date: 2022-09-19 21:33:40
  * @LastEditors: liziwei01
- * @LastEditTime: 2022-09-20 05:36:27
+ * @LastEditTime: 2022-09-20 08:42:55
  * @Description: file content
  */
 import { CheckWinner } from "../controllers/check_winner.js"
 
 function Init(router) {
-	router.post("/checkWinner", CheckWinner)
+	router.get("/checkWinner", CheckWinner)
 }
 
 export { Init }
