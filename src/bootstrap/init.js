2b5b6499e2a26424f7bba2f0509c555f70c9a028
liziwei <alssylk@gmail.com>
2022-09-20 09:19:30 -0400

    use cors

--- src/bootstrap/init.js
+++ src/bootstrap/init.js
@@ -2,11 +2,12 @@
  * @Author: liziwei01
  * @Date: 2022-09-19 23:51:51
  * @LastEditors: liziwei01
- * @LastEditTime: 2022-09-20 06:07:13
+ * @LastEditTime: 2022-09-20 09:18:55
  * @Description: file content
  */
 import express from "express"
 import bodyParser from "body-parser"
+import cors from "cors"
 // import * as redis from "../library/redis/redis.js"
 
 function InitMust() {
@@ -27,6 +28,7 @@ function InitRouter() {
 	// set basic middlewares
 	handler.use(bodyParser.urlencoded({extended:true}))
 	handler.use(bodyParser.json())
+	handler.use(cors())
 
 	return handler
 }
